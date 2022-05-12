21db8487305715b84e0241079f75ec24568d6c37
ali massoud <ali.masoud@lau.edu>
2022-05-12 20:00:56 +0200

    Extracting data and send back

---
+++ app.py
@@ -0,0 +1,48 @@
+# save this as app.py
+from cgitb import text
+from flask import Flask
+from flask import request, jsonify, json
+from flask_sqlalchemy import SQLAlchemy
+import os
+import psycopg2
+
+engine = psycopg2.connect(
+    database="postgres",
+    user="DSPDB",
+    password="PAUcwKtYQ2j0Tt0f16pe",
+    host="dspdb.c7yyz4ccfsai.eu-west-2.rds.amazonaws.com",
+    port='5432'
+)
+app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI']='postgresql://:@'
+SQLALCHEMY_TRACK_MODIFICATIONS = False
+db = SQLAlchemy(app)
+# dspdb.c7yyz4ccfsai.eu-west-2.rds.amazonaws.com
+class Tweet(db.Model):
+  __tablename__='Tweets'
+  id=db.Column(db.Integer,primary_key=True)
+  Email=db.Column(db.String(255))
+  Emergency_Email=db.Column(db.String(80))
+  Msg=db.Column(db.String(255))
+
+  def __init__(self,YourEmail,EmeEmail,Msg):
+    self.Email=YourEmail
+    self.Emergency_Email=EmeEmail
+    self.Msg=Msg
+
+
+@app.route("/Submit", methods=['GET'])
+def submit():
+    data = json.loads(request.data)
+    YourEmail= data['YourEmail']
+    EmeEmail=data['EmeEmail']
+    Msg=data['Msg']
+    # tweet = Tweet(YourEmail, EmeEmail,Msg)
+    # db.session.add(tweet)
+    # db.session.commit()
+    return  data
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
