4349c1caf2eb317efce368e8431464730102e165
Chetna Jayakumar <chetnaj043@gmail.com>
2022-05-27 12:33:05 +0200

    Updated python file

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask
 from flask import request, jsonify, json
 from flask_sqlalchemy import SQLAlchemy
 import pandas as pd
+#import numpy as np
 # import airflow
 app = Flask(__name__)
 
@@ -14,19 +15,27 @@ class Tweet(db.Model):
   id=db.Column(db.Integer,primary_key=True)
   Email=db.Column(db.String(255))
   Emergency_Email=db.Column(db.String(80))
-  Msg=db.Column(db.String(255))
+  text=db.Column(db.String(255))
   Prediction=db.Column(db.String(15))
 
-  def __init__(self,YourEmail,EmeEmail,Msg,Prediction):
+  def __init__(self,YourEmail,EmeEmail,text,Prediction):
     self.Email=YourEmail
     self.Emergency_Email=EmeEmail
-    self.Msg=Msg
+    self.text=text
     self.Prediction = Prediction
 
 
-def make_predictions():
+def predicted_outcome(df):
+    df1 = pd.DataFrame(df)
     # We add here our model code
-    return 'Danger'
+    df1['Prediction'] = 'Danger'
+    return df1
+# {
+#         "YourEmail": df1['YourEmail'],
+#         "EmeEmail": df1['EmeEmail'],
+#         "text": df1['text'],
+#         "Prediction": df1['Prediction']
+#     # }
 
 
 @app.route("/Submit", methods=['GET'])
@@ -34,16 +43,16 @@ def submit():
     data = json.loads(request.data)
     YourEmail= data['YourEmail']
     EmeEmail=data['EmeEmail']
-    Msg=data['Msg']
-    Prediction = make_predictions() # Pass my data to the model
-    tweet = Tweet(YourEmail, EmeEmail, Msg, Prediction)
+    text=data['text']
+    Prediction = predicted_outcome() # Pass my data to the model
+    tweet = Tweet(YourEmail, EmeEmail, text, Prediction)
     db.session.add(tweet)
     db.session.commit()
-    print(data)
+    # print(data)
     return {
     "YourEmail": YourEmail,
     "EmeEmail": EmeEmail,
-    "Msg": Msg,
+    "text": text,
     "Prediction": Prediction
     }
 
@@ -53,10 +62,10 @@ def index():
     if request.method == 'GET':
         saved_file = request.files['data_file']
         df = pd.read_csv(saved_file)
-        for index, row in df.iterrows():
-            print(row)
-            print(type(row))
-            break
+        g_list = pd.DataFrame()
+        result = predicted_outcome(df)
+        g_list = pd.concat([g_list,result], axis=0)
+        g_list.to_sql('Tweets',db)  ##Store the dataframe to DB
     return "Hello"
 
 
